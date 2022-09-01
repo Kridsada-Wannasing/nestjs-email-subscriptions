e6728558dcbc8b9e73b39394a35839cc16d0664f
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2022-09-01 18:13:37 +0700

    feat: set dependency and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SubscribersModule } from './subscribers/subscribers.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
+import Joi from '@hapi/joi';
 
 @Module({
-  imports: [SubscribersModule, DatabaseModule],
+  imports: [
+    SubscribersModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
