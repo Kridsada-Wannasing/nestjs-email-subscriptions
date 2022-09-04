f74408ec410fac0eda191e4f5e070c40c9489a5a
Kridsada-Wannasing <kridsada_aex002@hotmail.com>
2022-09-04 15:10:19 +0700

    feat: rabbitmq

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,29 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
+  const user = configService.get('RABBITMQ_USER');
+  const password = configService.get('RABBITMQ_PASSWORD');
+  const host = configService.get('RABBITMQ_HOST');
+  const queueName = configService.get('RABBITMQ_QUEUE_NAME');
+
+  // with TCP
+  // await app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.TCP,
+  //   options: {
+  //     port: configService.get('PORT'),
+  //   },
+  // });
+
+  // with RMQ
   await app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.TCP,
+    transport: Transport.RMQ,
     options: {
-      port: configService.get('PORT'),
+      urls: [`amqp://${user}:${password}@${host}`],
+      queue: queueName,
+      noAck: false,
+      queueOptions: {
+        durable: true,
+      },
     },
   });
 
